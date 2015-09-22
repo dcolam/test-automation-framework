b9e36673d8f13537c80d76003ee98d8ad264dbc8
fdambrine <fdambrine@14a2a948-07b1-4555-91b1-e264572feb61>
2015-09-22 13:34:10 +0000

    petit problème dans le launch
    git-svn-id: svn://devlnx00.goto.fr/qualification/test_automation_framework@54807 14a2a948-07b1-4555-91b1-e264572feb61

--- bin/testlinktool/main/lauchtestlinktests.py
+++ bin/testlinktool/main/lauchtestlinktests.py
@@ -6,13 +6,12 @@ TESTLINK_SERVER = "http://127.0.0.1/testlink/lib/api/xmlrpc/v1/xmlrpc.php"
 TESTLINK_PROJECT_ID = 1
 TESTLINK_PLATFORM_NAME = "TEST"
 MUST_CREATE_BUILD = True
-try:
-    from config import *
-except ImportError:
-    print("Warning we are using default parameters")
 
-
-if __name__ == "__main__":
+def lauch():
+    try:
+        from config import *
+    except ImportError:
+        print("Warning we are using default parameters")
     defaultTestLoader = TestLinkTestLoader()
     main(module=None,
          testRunner=TestLinkRunner(TESTLINK_SERVER, TESTLINK_PROJECT_ID, TESTLINK_PLATFORM_NAME,
