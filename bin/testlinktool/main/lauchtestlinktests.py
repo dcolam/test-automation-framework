6a6bd09e80205ee960e54d04aa59c8b3d07b7219
fdambrine <fdambrine@14a2a948-07b1-4555-91b1-e264572feb61>
2015-09-22 13:46:08 +0000

    support du config.py
    git-svn-id: svn://devlnx00.goto.fr/qualification/test_automation_framework@54808 14a2a948-07b1-4555-91b1-e264572feb61

--- bin/testlinktool/main/lauchtestlinktests.py
+++ bin/testlinktool/main/lauchtestlinktests.py
@@ -5,11 +5,17 @@ TESTLINK_API_KEY = ""
 TESTLINK_SERVER = "http://127.0.0.1/testlink/lib/api/xmlrpc/v1/xmlrpc.php"
 TESTLINK_PROJECT_ID = 1
 TESTLINK_PLATFORM_NAME = "TEST"
-MUST_CREATE_BUILD = True
+MUST_CREATE_BUILD = True 
 
-def lauch():
+def lauch(config_module=None):
     try:
-        from config import *
+        if config_module is not None:
+            TESTLINK_SERVER = getattr(config_module, "TESTLINK_SERVER")
+            TESTLINK_PROJECT_ID = getattr(config_module, "TESTLINK_PROJECT_ID")
+            TESTLINK_PLATFORM_NAME = getattr(config_module, "TESTLINK_PLATFORM_NAME")
+            TESTLINK_API_KEY = getattr(config_module, "TESTLINK_API_KEY")
+            MUST_CREATE_BUILD = getattr(config_module, "MUST_CREATE_BUILD")
+            
     except ImportError:
         print("Warning we are using default parameters")
     defaultTestLoader = TestLinkTestLoader()
