46cfeb2be66337fa7964f1f02cf19faad4ae61d1
fdambrine <francois.dambrine@vade-retro.com>
2016-03-08 11:10:55 +0100

    met à jour le setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,29 @@ setup(name='testlink_runner',
       version='1.0.1',
       description='Unittest wrapper to create interactions between testlink and python tests',
       author='Vade Retro technology',
+      author_email="support@vade-retro.com",
       url='',
+      keywords=["test", "reporting", "unittest"],
+      classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
+      long_description="""
+      This library aims to create a bridge between unittest and testlink.
+
+      As Testlink deals with integration and system testing more than bare unit tests, we include a simple API to
+      add assertions related to your UI. This uses selenium.
+      """,
       install_requires=[
-        "TestLink-API-Python-client",
-        "selenium",
-        "xvfbwrapper",
-        "lxml"
+        "TestLink-API-Python-client==0.6.2",
+        "selenium==2.52.0",
+        "xvfbwrapper==0.2.8",
+        "lxml==3.5.0"
       ],
       packages=['testlinktool.wrapper', 'testlinktool.main'],
       entry_points={
