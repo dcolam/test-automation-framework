ed3a5d1258e6d15d0e5af9b27af70b92894890f2
fdambrine <francois.dambrine@vade-retro.com>
2016-03-14 12:32:04 +0100

    ajout de la licence

--- setup.py
+++ setup.py
@@ -1,6 +1,39 @@
+"""
+
+Copyright (c) 201x "Vade Retro Technology"
+
+...
+
+
+This file is part of test-automation-framework.
+
+
+xxxxx is free software: you can redistribute it and/or modify
+
+it under the terms of the GNU General Public License as published by
+
+the Free Software Foundation, either version 3 of the License, or
+
+(at your option) any later version.
+
+
+This program is distributed in the hope that it will be useful,
+
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+
+GNU General Public License for more details.
+
+
+You should have received a copy of the GNU General Public License
+
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
 from setuptools import setup
 
-setup(name='testlink_runner',
+setup(name='test-automation-framework',
       version='1.0.1',
       description='Unittest wrapper to create interactions between testlink and python tests',
       author='Vade Retro technology',
@@ -12,7 +45,7 @@ setup(name='testlink_runner',
         "Programming Language :: Python :: 3",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules"
         ],
--- testlinktool/main/tests/test_with_selenium.py
+++ testlinktool/main/tests/test_with_selenium.py
@@ -1,3 +1,36 @@
+"""
+
+Copyright (c) 201x "Vade Retro Technology"
+
+...
+
+
+This file is part of test-automation-framework.
+
+
+xxxxx is free software: you can redistribute it and/or modify
+
+it under the terms of the GNU General Public License as published by
+
+the Free Software Foundation, either version 3 of the License, or
+
+(at your option) any later version.
+
+
+This program is distributed in the hope that it will be useful,
+
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+
+GNU General Public License for more details.
+
+
+You should have received a copy of the GNU General Public License
+
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
