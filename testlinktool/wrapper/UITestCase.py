4c9607098b28e59df4f0dcd681484a1ef11c81e9
fdambrine <francois.dambrine@vade-retro.com>
2016-03-14 12:32:04 +0100

    ajout de la licence

--- testlinktool/wrapper/UITestCase.py
+++ testlinktool/wrapper/UITestCase.py
@@ -1,11 +1,39 @@
-from time import sleep
+"""
+
+Copyright (c) 201x "Vade Retro Technology"
+
+...
+
+
+This file is part of test-automation-framework.
+
+
+xxxxx is free software: you can redistribute it and/or modify
+
+it under the terms of the GNU General Public License as published by
+
+the Free Software Foundation, either version 3 of the License, or
+
+(at your option) any later version.
+
+
+This program is distributed in the hope that it will be useful,
+
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+
+GNU General Public License for more details.
+
+
+You should have received a copy of the GNU General Public License
+
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
 import unittest
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from testlinktool.wrapper.TestLinkReport import TestLinkTestCase
 from selenium.webdriver.support import expected_conditions as EC
