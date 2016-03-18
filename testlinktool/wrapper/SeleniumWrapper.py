272dd5b798744ac5bde96b3c0d661f2c7833daa5
fdambrine <francois.dambrine@vade-retro.com>
2016-03-18 16:15:56 +0100

    better test

--- testlinktool/wrapper/SeleniumWrapper.py
+++ testlinktool/wrapper/SeleniumWrapper.py
@@ -39,7 +39,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoSuchWindowException
 import logging
 _log = logging.getLogger("testlinktool.selinum")
 
--- tests/test_waiting_elements.py
+++ tests/test_waiting_elements.py
@@ -35,6 +35,7 @@ from unittest import TestCase
 
 from os.path import dirname, join
 
+import time
 from selenium.webdriver.common.by import By
 
 from testlinktool.wrapper.UITestCase import UITestCase
@@ -53,8 +54,38 @@ class WaitingElement(TestCase):
         self.tested_object.get_phantomjs()
         self.tested_object.driver.get("file://" + join(dirname(__file__), "html_file.html"))
 
-        self.assertTrue(self.tested_object.element_does_not_appear_after_waiting(By.ID, "timeouted", 5))
+        self.assertTrue(self.tested_object.element_does_not_appear_after_waiting(By.ID, "hidden", 5))
 
-    def tearDown(self):
-        if hasattr(self, "tested_object"):
-            self.tested_object.close_driver()
+    def test_waiting_element_discovered_after_sometime(self):
+        self.tested_object = UITestCase()
+        self.tested_object.get_phantomjs()
+        self.tested_object.driver.get("file://" + join(dirname(__file__), "html_file.html"))
+
+        self.assertTrue(self.tested_object.element_does_not_appear(By.ID, "timeouted"))
+        self.assertFalse(self.tested_object.element_does_not_appear_after_waiting(By.ID, "timeouted", 30))
+
+
+class TestSelection(TestCase):
+
+    def test_select_with_value(self):
+        self.tested_object = UITestCase()
+        self.tested_object.get_phantomjs()
+        self.tested_object.driver.get("file://" + join(dirname(__file__), "html_file.html"))
+        self.tested_object.select_option_with_text(By.NAME, "selected", value="1")
+        self.tested_object.wait_to_be_clickable_then_click(By.NAME, "subform", 1)
+        self.assertIn("selected=1", self.tested_object.driver.current_url)
+
+    def test_select_with_text(self):
+        self.tested_object = UITestCase()
+        self.tested_object.get_phantomjs()
+        self.tested_object.driver.get("file://" + join(dirname(__file__), "html_file.html"))
+        self.tested_object.select_option_with_text(By.NAME, "selected", visible_text="number1")
+        self.tested_object.wait_to_be_clickable_then_click(By.NAME, "subform", 1)
+        self.assertIn("selected=1", self.tested_object.driver.current_url)
+
+    def test_select_not_existing(self):
+        self.tested_object = UITestCase()
+        self.tested_object.get_phantomjs()
+        self.tested_object.driver.get("file://" + join(dirname(__file__), "html_file.html"))
+        self.assertRaises(Exception, self.tested_object.select_option_with_text, By.NAME, "selected", value="9")
+
