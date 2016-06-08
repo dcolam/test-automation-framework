f0d428089f528b99ee9c0f6f44d2dd0ffdcdd3cd
fdambrine <francois.dambrine@vade-retro.com>
2016-06-08 13:21:34 +0200

    Fix bug in XML reporting

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ with open("requirements.txt") as req_file:
     requirements = list(filter(None, req_file.read().split("\n")))
 
 setup(name='test-automation-framework',
-      version='1.0.0',
+      version='1.0.1',
       description='Unittest wrapper to create interactions between testlink and python tests',
       author='Vade Retro technology',
       author_email="support@vade-retro.com",
